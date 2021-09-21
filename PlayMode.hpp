7e1946ec13c793a5d9e667ad943764b979c07bff
Chen, Hao <darkray@126.com>
2021-09-20 20:15:13 -0400

    [fix]Fixed Linux build failure
    g++ seems not to allow non-trival types as constexpr static class
    members and getting in-class initialized.
    
    Signed-off-by: Chen, Hao <darkray@126.com>

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -42,15 +42,15 @@ struct PlayMode : Mode {
 	constexpr static float SUCCESS_ROTATION_DELTA = 0.05f;
 
 	constexpr static float iss_rotation_increment = 4.f;
-	constexpr static glm::vec3 iss_forward_direction = glm::vec3(0.f, 1.f, 0.f);
+	glm::vec3 iss_forward_direction = glm::vec3(0.f, 1.f, 0.f);
 	constexpr static float orbit_rotation_increment = 0.04f;
-	constexpr static glm::vec3 orbit_rotation_axis = glm::vec3(0.f, 0.f, 1.f);
+	glm::vec3 orbit_rotation_axis = glm::vec3(0.f, 0.f, 1.f);
 	constexpr static float earth_rotation_increment = 0.02f;
-	constexpr static glm::vec3 earth_rotation_axis = glm::vec3(0.f, 0.f, 1.f);
+	glm::vec3 earth_rotation_axis = glm::vec3(0.f, 0.f, 1.f);
 	
 	constexpr static float camera_rotation_increment = 0.02f;
 	constexpr static float camera_move_increment = 0.01f;
-	constexpr static glm::vec3 camera_forward_direction = glm::vec3(0.f, 0.f, 1.f);
+	glm::vec3 camera_forward_direction = glm::vec3(0.f, 0.f, 1.f);
 
 	//transform states:
 	Scene::Transform *earth = nullptr;
